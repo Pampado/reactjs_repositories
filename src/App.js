81ff649208905b3ca71571e2a0d98c6ab776e3f6
pampado <pampado@hotmail.com>
2020-04-18 01:08:45 -0400

    Desafio concluído

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React from "react"
+
+import "./styles.css"
+import { useState } from "react"
+import { useEffect } from "react"
+import api from "./services/api"
+
+function App() {
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    api.get('/repositories').then(response => {
+      setRepositories(response.data)
+    })
+  }, [])
+
+  async function handleAddRepository() {
+    const response = await api.post('repositories', {
+      title: `Desafio 3 Front-end ${Date.now()}`,
+	    url: "http://github.com/Pampado/desafio3/",
+	    techs: [
+        "Node.js",
+        "JavaScript",
+        "ReactJS"
+	    ]
+    })
+
+    const repository = response.data
+
+    setRepositories([...repositories, repository])
+  }
+
+  async function handleRemoveRepository(id) {
+    const response = await api.delete(`repositories/${id}`)
+
+    setRepositories(repositories.filter((repository) => (repository.id !== id)))
+  }
+
+  return (
+    <div>
+      <ul data-testid="repository-list">
+        {repositories.map(repository => 
+          <li key={repository.id}>{repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>)}
+      </ul>
+
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
+  );
+}
+
+export default App
